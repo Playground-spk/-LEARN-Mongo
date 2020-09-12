7102c31ae34b304c736505bc57de260f9ef60db3
spk-cpsamut <taresupakon@gmail.com>
2020-09-12 23:16:07 +0700

    [UPDATE] api get all tests have test about pagination

--- controllers/Test.js
+++ controllers/Test.js
@@ -40,7 +40,11 @@ const getAllTest = async (req, res) => {
 
     // 4) Pagination
 
-    query = query.skip(2).limit(10);
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
 
     /* execute query */
     const tests = await query;
